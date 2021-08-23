18ddd4f295e5a4f6e694afe2d86d1c61b8a073a9
Sivanidevarapalli26 <47571962+Sivanidevarapalli26@users.noreply.github.com>
2021-08-23 17:53:04 +0530

    Update assignment1.c

--- Assignment-1/codes/assignment1.c
+++ Assignment-1/codes/assignment1.c
@@ -1,18 +1,24 @@
-#include <stdio.h>
-
-int main() { 
-    int arr [4] [5];
-    int i, j;
-    for (i=0; i<4; i++){
-        for (j=0; j<5; j++){ 
-            arr[i][j] = 10*i + j;
+#include<stdio.h>
+int main(){
+    int arr[4][5] ;
+    int i,j;
+    for(i=0;i<4;i++){
+        for(j=0;j<5;j++){
+            arr[i][j]=10*i+j;
             
         }
         
     }
+    int *ptr = &arr;
 
-printf("%d", *(arr[1] + 9));
-
+    //accessing the elements of 2D array using ptr
+    for(int i=0;i<4;i++){
+            for(int j=0;j<5;j++){
+            printf("%d ",*((ptr+i*5)+j)); //5 is the number of columns
+            //*((ptr+i*5)+j) is similar to arr[i][j]
+            }
+            printf("\n");
+    }
+printf("%d",*(arr[1]+9));
 return 0;
-
 }
