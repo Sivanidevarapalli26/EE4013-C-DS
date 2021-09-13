11ce6a8fc04a82baf32394a5927c7b13918b1773
Sivanidevarapalli26 <47571962+Sivanidevarapalli26@users.noreply.github.com>
2021-09-13 09:30:39 +0530

    Update assignment1.c

--- Assignment-1/codes/assignment1.c
+++ Assignment-1/codes/assignment1.c
@@ -1,24 +1,29 @@
 #include<stdio.h>
-int main(){
-    int arr[4][5] ;
-    int i,j;
-    for(i=0;i<4;i++){
-        for(j=0;j<5;j++){
-            arr[i][j]=10*i+j;
-            
-        }
-        
-    }
-    int *ptr = &arr;
+#include<stdlib.h>
 
-    //accessing the elements of 2D array using ptr
-    for(int i=0;i<4;i++){
-            for(int j=0;j<5;j++){
-            printf("%d ",*((ptr+i*5)+j)); //5 is the number of columns
-            //*((ptr+i*5)+j) is similar to arr[i][j]
-            }
-            printf("\n");
+int main(){
+    //No of rows(r)=4 and no of cols(c)=5
+    int r=4,c=5,i,j;
+    //Dynamically allocating a matrix in heap using malloc function
+    int **arr = (int **)malloc(r * sizeof(int *));
+    for (i=0; i<r; i++)
+         arr[i] = (int *)malloc(c * sizeof(int));
+         
+    //Allocating values of the matrix     
+    // Note that arr[i][j] is same as *(*(arr+i)+j)
+    for (int i = 0; i <  r; i++)
+      for (int j = 0; j < c; j++)
+         arr[i][j]=10*i+j;
+         
+    //printing the matrix elements    
+    for (i = 0; i <  r; i++){
+      for (j = 0; j < c; j++){
+         printf("%d ", arr[i][j]);
+      }
+      printf("\n");
     }
-printf("%d",*(arr[1]+9));
-return 0;
+    //Required element address=first element in second row address + 9 + 3(diff from row end to next row start)
+    printf("%d",*(arr[1]+9+3));
+    return 0;
 }
+
